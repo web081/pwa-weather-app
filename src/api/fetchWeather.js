bd391938151c1d28275f046fa6e27b8753e6795e
bom234 <bomprince@gmail.com>
2020-06-15 11:30:44 +0100

    update

---
+++ src/api/fetchWeather.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const URL = 'https://api.openweathermap.org/data/2.5/weather';
+const API_KEY = 'cbe98e18c20cec4007d9b18fd91798db';
+export const fetchWeather = async (query) => {
+  const { data } = await axios.get(URL, {
+    params: {
+      q: query,
+      units: 'metric',
+      APPID: API_KEY,
+    },
+  });
+  return data;
+};
